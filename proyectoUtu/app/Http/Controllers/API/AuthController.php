e05ed643c742893673b469a3f49a1a8fc4609ec2
NeoDau <neodau@gmail.com>
2025-07-22 17:41:14 -0300

    base de datos + controllers

---
+++ proyectoUtu/app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $user = User::create([
+            'full_name' => $request->full_name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Usuario registrado',
+            'user' => $user
+        ]);
+    }
+}
